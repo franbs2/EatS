78ff8707f64ebb93cf3f8e944b684747509f50a1
Artur Matos Malcher de Sousa <arturmatosmalcherdesousa@gmail.com>
2024-08-08 19:43:01 -0300

    feat: Add Provider and Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:eats/presentation/style/color.dart' as color;
+import 'package:eats/providers/user_provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'presentation/view/home_page.dart';
@@ -20,41 +24,48 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        textTheme: GoogleFonts.soraTextTheme(
-          Theme.of(context).textTheme,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider()
         ),
-      ),
-      home: const HomePage(),
-      debugShowCheckedModeBanner: false,
-      
-      // To configure the Firebase Persisting Auth State, use the following code:
-      //
-      // home: StreamBuilder(
-      //   stream: FirebaseAuth.instance.authStateChanges(),
-      //   builder: (context, snapshot) {
-      //     if(snapshot.connectionState == ConnectionState.active) {
-      //       if(snapshot.hasData) {
-      //         return const HomePage();
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          textTheme: GoogleFonts.soraTextTheme(
+            Theme.of(context).textTheme,
+          ),
+        ),
+        // home: const HomePage(),
+        // debugShowCheckedModeBanner: false,
 
-      //       } else if(snapshot.hasError) {
-      //         return Center(
-      //           child: Text('${snapshot.error}'),
-      //         );
-      //       }
-      //     }
-      //     if(snapshot.connectionState == ConnectionState.waiting) {
-      //       return const Center(
-      //         child: CircularProgressIndicator(
-      //           color: AppTheme.primaryColor,
-      //         )
-      //       );
-      //     }
+        //To configure the Firebase Persisting Auth State, use the following code:
+        
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.active) {
+              if(snapshot.hasData) {
+                return const HomePage();
 
-      //     return const HomePage();
-      //   },
-      // ),
+              } else if(snapshot.hasError) {
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              }
+            }
+            if(snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: color.AppTheme.primaryColor,
+                )
+              );
+            }
+
+            return const HomePage();
+          },
+        ),
+      )
     );
   }
 }
