86e9b71b111bcb9635c1f0df11ec6b61ca423b33
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-08-08 22:07:46 -0300

    refactor: Refactor default authentication page layout

--- lib/presentation/widget/page_default_auth.dart
+++ lib/presentation/widget/page_default_auth.dart
@@ -11,48 +11,46 @@ class PageDefaultAuth extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: SingleChildScrollView(
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.3,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.bottomLeft,
-                    colors: [Color(0xffEFC136), Color(0xff539F33)]),
-              ),
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.2,
-                  child: Image.asset('assets/logo_white.png'),
-                ),
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.3,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  colors: [Color(0xffEFC136), Color(0xff539F33)]),
+            ),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Image.asset('assets/logo_white.png'),
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.2),
-              padding: const EdgeInsets.all(20),
-              height: MediaQuery.of(context).size.height * 0.7,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40),
-                  topRight: Radius.circular(40),
-                ),
+          ),
+          Column(
+            children: [
+              Expanded(
+                child: Container(
+                    margin: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.height * 0.2),
+                    padding: const EdgeInsets.all(20),
+                    height: MediaQuery.of(context).size.height * 0.7,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40),
+                      ),
+                    ),
+                    child: body),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [body],
-              ),
-            )
-          ],
-        ),
-      )),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
