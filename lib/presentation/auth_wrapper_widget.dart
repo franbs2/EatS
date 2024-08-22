963bdec983481526aaf25d6c2550c35d83de44c7
Artur Matos Malcher de Sousa <arturmatosmalcherdesousa@gmail.com>
2024-08-21 22:56:59 -0300

    fix: auth_wrapper_widget with StreamBuilder

--- lib/presentation/auth_wrapper_widget.dart
+++ lib/presentation/auth_wrapper_widget.dart
@@ -1,37 +1,43 @@
-import 'package:eats/data/model/user.dart';
 import 'package:eats/presentation/providers/user_provider.dart';
 import 'package:eats/presentation/view/home_page.dart';
 import 'package:eats/presentation/view/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fb;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+
 class AuthWrapperWidget extends StatelessWidget {
   const AuthWrapperWidget({super.key});
+@override
+Widget build(BuildContext context) {
+  return StreamBuilder<fb.User?>(
+    stream: fb.FirebaseAuth.instance.authStateChanges(),
+    builder: (context, snapshot) {
+      final user = snapshot.data;
+      final userProvider = Provider.of<UserProvider>(context, listen: false);
 
-  @override
-  Widget build(BuildContext context) {
-    final user = Provider.of<User?>(context);
-    final userProvider = Provider.of<UserProvider>(context, listen: false);
+      if (user == null) {
+        return LoginPage();
+      } else {
+        return FutureBuilder(
+          future: userProvider.refreshUser(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Scaffold(
+                body: Center(
+                    child: CircularProgressIndicator(color: Color(0xff529536))),
+              );
+            } else if (snapshot.hasError) {
+              return const Scaffold(
+                body: Center(child: Text('Erro ao carregar os dados do usuário')),
+              );
+            }
+            return const HomePage();
+          },
+        );
+      }
+    },
+  );
+}
 
-    if (user == null) {
-      return LoginPage();
-    } else {
-      return FutureBuilder(
-        future: userProvider.refreshUser(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(
-              body: Center(
-                  child: CircularProgressIndicator(color: Color(0xff529536))),
-            );
-          } else if (snapshot.hasError) {
-            return const Scaffold(
-              body: Center(child: Text('Erro ao carregar os dados do usuário')),
-            );
-          }
-          return const HomePage();
-        },
-      );
-    }
-  }
 }
