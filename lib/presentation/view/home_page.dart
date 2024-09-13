f15618ca075494fa80a9b45a8322762ab2bb17a8
Artur Matos Malcher de Sousa <arturmatosmalcherdesousa@gmail.com>
2024-09-13 01:52:55 -0300

    refactor: home_page

--- lib/presentation/view/home_page.dart
+++ lib/presentation/view/home_page.dart
@@ -22,7 +22,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final TextEditingController _searchController = TextEditingController();
-  final ValueNotifier<bool> _isEmptyNotifier = ValueNotifier(true);
 
   @override
   void initState() {
