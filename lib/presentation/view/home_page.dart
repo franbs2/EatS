5ce9f20e62fa83aa36322d6c7b409efa32c6abe2
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-09-24 00:55:12 -0300

    feat: Refresh

--- lib/presentation/view/home_page.dart
+++ lib/presentation/view/home_page.dart
@@ -29,9 +29,14 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     // Busca receitas e banners após o layout ser construído.
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      Provider.of<RecipesProvider>(context, listen: false).fetchRecipes(null);
-      Provider.of<BannersProvider>(context, listen: false).fetchBanners();
+      _fetchData();
     });
+  }
+
+  Future<void> _fetchData() async {
+    await Provider.of<RecipesProvider>(context, listen: false)
+        .fetchRecipes(null);
+    await Provider.of<BannersProvider>(context, listen: false).fetchBanners();
   }
 
   void _searchRecipes(String query) {
@@ -63,154 +68,158 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
-          child: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                      top: 30, left: 30, right: 30, bottom: 20),
-                  child: Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Olá,',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: AppTheme.secondaryColor,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            userProvider.user!.username,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: AppTheme.secondaryColor,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Spacer(),
-                      const ImgPerfilWidget(),
-                    ],
-                  ),
-                ),
-              ),
-              SliverAppBar(
-                backgroundColor: const Color.fromARGB(0, 255, 255, 255),
-                collapsedHeight: MediaQuery.of(context).size.height * 0.1,
-                pinned: true,
-                flexibleSpace: Center(
+          child: RefreshIndicator(
+            onRefresh:
+                _fetchData, // Chama a função para buscar dados ao atualizar
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 26.0),
-                    child: SearchBarWidget(
-                      controller: _searchController,
-                      onChanged: (value) {
-                        // Implementando debounce para a busca
-                        Future.delayed(
-                          const Duration(milliseconds: 800),
-                          () => _searchRecipes(value),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ),
-
-              // Renderiza o banner e o slider de banners.
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 12),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [Colors.transparent, Color(0xffF9F9F9)],
-                        stops: [0.5, 0.5],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                      ),
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Color(0xffF9F9F9),
-                          width: 2.0,
-                        ),
-                      ),
-                    ),
-                    child: Consumer<BannersProvider>(
-                      builder: (context, bannersProvider, child) {
-                        return CarouselWidget(banners: bannersProvider.banners);
-                      },
-                    ),
-                  ),
-                ),
-              ),
-              // Renderiza as categorias de filtro.
-              SliverToBoxAdapter(
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minHeight: MediaQuery.of(context).size.height * 0.6,
-                  ),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Color(0xffF9F9F9),
-                    ),
-                    child: Column(
+                    padding: const EdgeInsets.only(
+                        top: 30, left: 30, right: 30, bottom: 20),
+                    child: Row(
                       children: [
-                        const SizedBox(height: 16),
-                        const FilterCategoriesRecipesWidget(
-                            categories: categories),
-                        Consumer<RecipesProvider>(
-                          builder: (context, recipesProvider, child) {
-                            if (recipesProvider.isLoading) {
-                              return const Center(
-                                child: CircularProgressIndicator(
-                                  color: AppTheme.primaryColor,
-                                ),
-                              );
-                            }
-                            if (recipesProvider.errorMessage != null) {
-                              return Center(
-                                child: Column(
-                                  children: [
-                                    const SizedBox(height: 24),
-                                    Text(
-                                      recipesProvider.errorMessage!
-                                          .toLowerCase(),
-                                      style: const TextStyle(
-                                        color: AppTheme.primaryColor,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 24),
-                                    Image.asset(
-                                      ImageApp.hungryIllustration,
-                                      height:
-                                          MediaQuery.of(context).size.height *
-                                              0.2,
-                                    ),
-                                  ],
-                                ),
-                              );
-                            }
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 24, vertical: 10),
-                              child: GridViewRecipesWidget(
-                                listRecipes: recipesProvider.recipes,
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Olá,',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: AppTheme.secondaryColor,
+                                fontWeight: FontWeight.normal,
                               ),
-                            );
-                          },
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              userProvider.user!.username,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: AppTheme.secondaryColor,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
                         ),
+                        const Spacer(),
+                        const ImgPerfilWidget(),
                       ],
                     ),
                   ),
                 ),
-              ),
-            ],
+                SliverAppBar(
+                  backgroundColor: const Color.fromARGB(0, 255, 255, 255),
+                  collapsedHeight: MediaQuery.of(context).size.height * 0.1,
+                  pinned: true,
+                  flexibleSpace: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 26.0),
+                      child: SearchBarWidget(
+                        controller: _searchController,
+                        onChanged: (value) {
+                          // Implementando debounce para a busca
+                          Future.delayed(
+                            const Duration(milliseconds: 800),
+                            () => _searchRecipes(value),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                // Renderiza o banner e o slider de banners.
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 12),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Colors.transparent, Color(0xffF9F9F9)],
+                          stops: [0.5, 0.5],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        ),
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color(0xffF9F9F9),
+                            width: 2.0,
+                          ),
+                        ),
+                      ),
+                      child: Consumer<BannersProvider>(
+                        builder: (context, bannersProvider, child) {
+                          return CarouselWidget(
+                              banners: bannersProvider.banners);
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                // Renderiza as categorias de filtro.
+                SliverToBoxAdapter(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: MediaQuery.of(context).size.height * 0.6,
+                    ),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Color(0xffF9F9F9),
+                      ),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 16),
+                          const FilterCategoriesRecipesWidget(
+                              categories: categories),
+                          Consumer<RecipesProvider>(
+                            builder: (context, recipesProvider, child) {
+                              if (recipesProvider.isLoading) {
+                                return const Center(
+                                  child: CircularProgressIndicator(
+                                    color: AppTheme.primaryColor,
+                                  ),
+                                );
+                              }
+                              if (recipesProvider.errorMessage != null) {
+                                return Center(
+                                  child: Column(
+                                    children: [
+                                      const SizedBox(height: 24),
+                                      Text(
+                                        recipesProvider.errorMessage!
+                                            .toLowerCase(),
+                                        style: const TextStyle(
+                                          color: AppTheme.primaryColor,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 24),
+                                      Image.asset(
+                                        ImageApp.hungryIllustration,
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                0.2,
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+                              return Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 24, vertical: 10),
+                                child: GridViewRecipesWidget(
+                                  listRecipes: recipesProvider.recipes,
+                                ),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
