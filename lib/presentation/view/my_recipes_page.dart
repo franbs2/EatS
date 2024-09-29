f339befcd96e931515691f0f895891d3d986afaf
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-09-28 22:17:48 -0300

    style: safearea my recipe

--- lib/presentation/view/my_recipes_page.dart
+++ lib/presentation/view/my_recipes_page.dart
@@ -42,82 +42,86 @@ class _MyRecipesPageState extends State<MyRecipesPage> {
 
     return Scaffold(
       backgroundColor: AppTheme.backgroundColor,
-      body: Column(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.bottomLeft,
-                colors: [Color(0xffEFC136), Color(0xff539F33)],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  colors: [Color(0xffEFC136), Color(0xff539F33)],
+                ),
               ),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.all(24),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Minhas Receitas',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
+              child: const Padding(
+                padding: EdgeInsets.all(24),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Minhas Receitas',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: RefreshIndicator(
-              color: AppTheme.primaryColor,
-              onRefresh: _loadUserRecipes,
-              child: listRecipes == null
-                  ? const Center(child: CircularProgressIndicator())
-                  : listRecipes!.isEmpty
-                      ? const Center(child: Text('Nenhuma receita encontrada.'))
-                      : ListView.builder(
-                          itemCount: listRecipes!.length,
-                          itemBuilder: (context, index) {
-                            Recipes recipe = listRecipes![index];
-                            return FutureBuilder<String>(
-                              future: storageService.loadImageInURL(
-                                  recipe.image, true),
-                              builder: (context, snapshot) {
-                                if (snapshot.connectionState ==
-                                    ConnectionState.waiting) {
-                                  return const Center(
-                                    child: LinearProgressIndicator(
-                                      color: AppTheme.homeColorOne,
-                                      backgroundColor: AppTheme.secondaryColor,
-                                    ),
-                                  );
-                                } else if (snapshot.hasError) {
-                                  return const Center(
-                                      child: Text('Erro ao carregar imagem'));
-                                } else {
-                                  return RecipeCardListWidget(
-                                    recipe: recipe,
-                                    imageUrl: snapshot.data ?? '',
-                                    updatePage: _loadUserRecipes,
-                                  );
-                                }
-                              },
-                            );
-                          },
-                        ),
+            Expanded(
+              child: RefreshIndicator(
+                color: AppTheme.primaryColor,
+                onRefresh: _loadUserRecipes,
+                child: listRecipes == null
+                    ? const Center(child: CircularProgressIndicator())
+                    : listRecipes!.isEmpty
+                        ? const Center(
+                            child: Text('Nenhuma receita encontrada.'))
+                        : ListView.builder(
+                            itemCount: listRecipes!.length,
+                            itemBuilder: (context, index) {
+                              Recipes recipe = listRecipes![index];
+                              return FutureBuilder<String>(
+                                future: storageService.loadImageInURL(
+                                    recipe.image, true),
+                                builder: (context, snapshot) {
+                                  if (snapshot.connectionState ==
+                                      ConnectionState.waiting) {
+                                    return const Center(
+                                      child: LinearProgressIndicator(
+                                        color: AppTheme.homeColorOne,
+                                        backgroundColor:
+                                            AppTheme.secondaryColor,
+                                      ),
+                                    );
+                                  } else if (snapshot.hasError) {
+                                    return const Center(
+                                        child: Text('Erro ao carregar imagem'));
+                                  } else {
+                                    return RecipeCardListWidget(
+                                      recipe: recipe,
+                                      imageUrl: snapshot.data ?? '',
+                                      updatePage: _loadUserRecipes,
+                                    );
+                                  }
+                                },
+                              );
+                            },
+                          ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
